00d56971b0b3cf8ff5c428e9c41fc4e8c402288e
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2021-08-09 21:18:54 +0300

    done functional change theme

--- src/index.js
+++ src/index.js
@@ -1,4 +1,35 @@
 'use srticts'
 
-const a = document.querySelector('.theme-switch__control')
-console.log(a)
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+
+const inputChangeTheme = document.querySelector('.theme-switch__toggle')
+
+const bodyForTheme = document.querySelector('body')
+
+inputChangeTheme.addEventListener('change', function changeThemeForBodyByInput() {
+
+    if (inputChangeTheme.checked) {
+        bodyForTheme.setAttribute('class', Theme.DARK)
+        localStorage.setItem('Thems', Theme.DARK)
+
+
+    } else {
+        bodyForTheme.setAttribute('class', Theme.LIGHT)
+        localStorage.setItem('Thems', Theme.LIGHT)
+    }
+
+})
+
+const theme = localStorage.getItem('Thems');
+if (inputChangeTheme.checked = (theme == Theme.DARK)) {
+    bodyForTheme.setAttribute('class', Theme.DARK)
+    inputChangeTheme.checked === true
+}
+else {
+    bodyForTheme.setAttribute('class', Theme.LIGHT);
+    inputChangeTheme.checked === false
+}
+
