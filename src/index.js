fc73ac169ab0f3841b425d73b0bf1baeec32a583
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2021-08-09 22:53:57 +0300

    added markup width handelbars

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
 'use srticts'
+import menuTemplate from './templates/menu-cards.hbs';
+import menuArray from './menu.json';
+
+// добавление разметки на страницу с помощью шаблонизатора
+
+const createCardMenuMarkup = menuArray.map(menuTemplate).join('');
+const menuContainer = document.querySelector('.menu');
+menuContainer.insertAdjacentHTML('beforeend', createCardMenuMarkup)
+
+// Изменения темы сайта и работа с localStorage
+
+const inputChangeTheme = document.querySelector('.theme-switch__toggle')
+const bodyForTheme = document.querySelector('body')
+inputChangeTheme.addEventListener('change', changeThemeForBodyByInput)
 
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
 };
+// сохранение темы при перезагрузке
 
-const inputChangeTheme = document.querySelector('.theme-switch__toggle')
-
-const bodyForTheme = document.querySelector('body')
-
-inputChangeTheme.addEventListener('change', function changeThemeForBodyByInput() {
-
-    if (inputChangeTheme.checked) {
-        bodyForTheme.setAttribute('class', Theme.DARK)
-        localStorage.setItem('Thems', Theme.DARK)
-
-
-    } else {
-        bodyForTheme.setAttribute('class', Theme.LIGHT)
-        localStorage.setItem('Thems', Theme.LIGHT)
-    }
-
-})
-
-const theme = localStorage.getItem('Thems');
-if (inputChangeTheme.checked = (theme == Theme.DARK)) {
+const themeLocalStorage = localStorage.getItem('Theme');
+if (inputChangeTheme.checked = (themeLocalStorage == Theme.DARK)) {
     bodyForTheme.setAttribute('class', Theme.DARK)
     inputChangeTheme.checked === true
 }
@@ -32,4 +29,16 @@ else {
     bodyForTheme.setAttribute('class', Theme.LIGHT);
     inputChangeTheme.checked === false
 }
+// функция изменения темы при смене положения чекбокса
+function changeThemeForBodyByInput() {
+    if (inputChangeTheme.checked) {
+        bodyForTheme.setAttribute('class', Theme.DARK)
+        localStorage.setItem('Theme', Theme.DARK)
+    } else {
+        bodyForTheme.setAttribute('class', Theme.LIGHT)
+        localStorage.setItem('Theme', Theme.LIGHT)
+    }
+}
+
+
 
